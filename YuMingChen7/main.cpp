ba2fdbc2355b3f008e1ae68262b4e82f75c5c078
Sandpc <yumingccc@163.com>
2019-04-14 19:54:22 +0800

    YuMingChen 4次作业

---
+++ YuMingChen7/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int count;
+
+   bool breakOut = false;
+   for (count=1;(count<=10)&&(!breakOut);count++)
+   {
+      if (count==5)
+         breakOut=true;
+      else
+         cout<<count<<" ";
+   }
+}
